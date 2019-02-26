cfdea04993d8f300edfd62a440beddc9319dae0b
GSachdeva29 <sachdega@sheridancollege.ca>
2019-02-25 19:06:20 -0500

    Arraylist_Test on github

---
+++ src/Arraylist_Test.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Arraylist_Test
+{
+    public static void main(String[] args)
+    {
+       // ArrayList java2_session=new ArrayList();
+        ArrayList<Integer>java2_session =new ArrayList<Integer>();    //Generic
+        java2_session.add(5);
+        java2_session.add(3);
+        java2_session.add(6);
+        java2_session.add(0);
+
+        /*Iterator iter=java2_session.iterator();
+
+        while(iter.hasNext())
+        {
+            System.out.println(String.valueOf(iter.next()));
+        }*/
+        /*
+        //Generic
+        for (Integer x:java2_session)
+        {
+            System.out.println(x);
+        }
+        */
+        java2_session.forEach
+        (
+          x->
+          {
+              System.out.println(x);
+          }
+        );
+    }
+}
